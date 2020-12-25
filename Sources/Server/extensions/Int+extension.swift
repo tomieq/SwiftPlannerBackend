a466821dec051745cd8ef8ebec5f17df63ae29b6
Tomasz Kucharski <tomieq@wp.pl>
2020-12-25 22:58:01 +0100

    Add Int extension

---
+++ Sources/Server/extensions/Int+extension.swift
@@ -0,0 +1,30 @@
+//
+//  Int+extension.swift
+//  
+//
+//  Created by Tomasz Kucharski on 25/12/2020.
+//
+
+import Foundation
+
+extension Int {
+
+    var ordinal: String {
+        var suffix: String
+        let ones: Int = self % 10
+        let tens: Int = (self/10) % 10
+        if tens == 1 {
+            suffix = "th"
+        } else if ones == 1 {
+            suffix = "st"
+        } else if ones == 2 {
+            suffix = "nd"
+        } else if ones == 3 {
+            suffix = "rd"
+        } else {
+            suffix = "th"
+        }
+        return "\(self)\(suffix)"
+    }
+
+}
