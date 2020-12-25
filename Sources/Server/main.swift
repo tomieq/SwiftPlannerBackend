1576d992dfa74f92722eaf6e00ecdf27fe2bd46d
Tomasz Kucharski <tomieq@wp.pl>
2020-12-25 22:57:50 +0100

    Show working directory when starting app

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -1,4 +1,5 @@
 import Dispatch
+import Foundation
 
 let server = HttpServer()
 let application = WebApplication(server)
@@ -6,7 +7,7 @@ let application = WebApplication(server)
 let semaphore = DispatchSemaphore(value: 0)
 do {
     try server.start(9080, forceIPv4: true)
-    print("Server has started ( port = \(try server.port()) ). Try to connect now...")
+    print("Server has started ( port = \(try server.port()) dir = \(FileManager.default.currentDirectoryPath). Try to connect now...")
     semaphore.wait()
 } catch {
     print("Server start error: \(error)")
