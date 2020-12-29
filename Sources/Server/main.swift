32e8bf5000ac9a6b39f96b1dbe73fde8ad1c3536
Tomasz Kucharski <tomieq@wp.pl>
2020-12-29 13:22:14 +0100

    Change port to 5100

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -6,8 +6,8 @@ let application = WebApplication(server)
 
 let semaphore = DispatchSemaphore(value: 0)
 do {
-    try server.start(9080, forceIPv4: true)
-    print("Server has started ( port = \(try server.port()) dir = \(FileManager.default.currentDirectoryPath). Try to connect now...")
+    try server.start(5100, forceIPv4: true)
+    print("Server has started ( port = \(try server.port()) dir = \(FileManager.default.currentDirectoryPath)).")
     semaphore.wait()
 } catch {
     print("Server start error: \(error)")
